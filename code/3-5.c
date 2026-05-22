394b08dcebffd6cdfee2d8f69174a561c6540bc6
Lubeli <144096691+Lubeli@users.noreply.github.com>
2026-05-22 16:29:52 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 
-int main(int argc, const char* argv[]) {
+int main(int argc, const char* argv[]){
     const char* filename = "numbers.txt";
 
     FILE* fp = fopen(filename, "r");
-    if (fp == NULL) {
+    if(fp == NULL)
         return 0;
+    
+    int sum = 0;
+    int num;
+    char invalid[64];
+
+    while (!feof(fp)) {
+        if (fscanf(fp, "%d", &num) == 1)
+            sum += num;
+        else if (fscanf(fp, "%s", invalid) == 1)
+            fprintf(stderr, "invalid input %s\n", invalid);
     }
 
-
-    int sum = 0;
-
-
-    printf("sum: %d\n", sum);
+    printf("sum : %d\n", sum);
     fclose(fp);
     return 0;
 }
