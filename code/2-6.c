ab1fb95fb1012cf9de1e065174013c9151bdd1a4
Lubeli <144096691+Lubeli@users.noreply.github.com>
2026-05-22 16:31:58 +0900

    Update 2-6.c
    Refactor main function for improved readability and functionality.

--- code/2-6.c
+++ code/2-6.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, const char* argv[]) {
-    if (argc < 3)
+int main(int argc, const char* argv[]){
+    if(argc < 3)
         return 0;
 
     FILE* fp = fopen(argv[1], "r");
     const char* target_str = argv[2];
-
-
-    
+   
+    char line[1024];
+    while (fgets(line, sizeof(line), fp))
+        if (strstr(line, target_str)) printf("%s", line);
+   
     fclose(fp);
 }
-
